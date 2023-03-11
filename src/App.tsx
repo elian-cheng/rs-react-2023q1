b474698a71ae466cc7e683612c3e234c2a0b7e80
Olga Chernega <eliang.cheng@gmail.com>
2023-03-11 17:43:23 +0200

    init: start task react-components, add config files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main className="main">
+        <div className="main__container">React-Components</div>
+      </main>
+    );
+  }
+}
