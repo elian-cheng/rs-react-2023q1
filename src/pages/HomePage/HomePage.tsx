8aefd35bd1e18f335e828b64e66a78b274a2a6af
Olga Chernega <eliang.cheng@gmail.com>
2023-03-12 21:02:06 +0200

    feat: add search form and save input value to storage functionality

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import SearchForm from './components/SearchForm/SearchForm';
 
 export default class HomePage extends Component {
   render() {
-    return <div>HomePage</div>;
+    return (
+      <div className="catalog__wrapper">
+        <SearchForm />
+      </div>
+    );
   }
 }
---
+++ src/pages/HomePage/components/SearchForm/SearchForm.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+interface ISearchForm {
+  ElyteSearch: string;
+}
+
+export default class SearchForm extends Component<object, ISearchForm> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      ElyteSearch: localStorage.getItem('ElyteSearch') || '',
+    };
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+
+  componentDidMount() {
+    const initialSearchValue = localStorage.getItem('ElyteSearch') || '';
+    this.setState({ ElyteSearch: initialSearchValue });
+  }
+
+  componentWillUnmount() {
+    this.saveToStorage(this.state.ElyteSearch);
+  }
+
+  saveToStorage(item: string) {
+    localStorage.setItem('ElyteSearch', item);
+  }
+
+  handleSearchChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ ElyteSearch: e.target.value });
+    this.saveToStorage(e.target.value);
+  }
+
+  render(): JSX.Element {
+    return (
+      <form action="" className="search-form">
+        <input
+          className="search-form__input"
+          type="search"
+          placeholder="Search product here..."
+          value={this.state.ElyteSearch}
+          onChange={this.handleSearchChange}
+        />
+      </form>
+    );
+  }
+}
