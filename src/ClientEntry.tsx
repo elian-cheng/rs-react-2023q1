e4391481cbfb3d9ef878187aeb277d8765e17379
Olga Chernega <eliang.cheng@gmail.com>
2023-04-25 15:54:55 +0300

    fix: remove comments

--- src/ClientEntry.tsx
+++ src/ClientEntry.tsx
@@ -5,17 +5,11 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { hydrateRoot } from 'react-dom/client';
 
-// import Loader from 'components/Loader/Loader';
-// const AppComponent = React.lazy(() => import('./App'));
-
 hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {/* <React.Suspense fallback={<Loader />}>
-          <AppComponent />
-        </React.Suspense> */}
         <App />
       </BrowserRouter>
     </Provider>
--- src/ServerEntry.tsx
+++ src/ServerEntry.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 
-import {
-  PipeableStream,
-  renderToPipeableStream,
-  RenderToPipeableStreamOptions,
-} from 'react-dom/server';
+import { renderToPipeableStream } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import type { Request, Response } from 'express';
 
 import { StaticRouter } from 'react-router-dom/server';
 import store from 'store';
 import App from './App';
-
-import Loader from 'components/Loader/Loader';
-const AppComponent = React.lazy(() => import('./App'));
 
 export function render(req: Request, res: Response, template: string) {
   const html = template.split('<!--app-html-->');
@@ -23,9 +16,6 @@ export function render(req: Request, res: Response, template: string) {
     <React.StrictMode>
       <Provider store={store}>
         <StaticRouter location={req.url}>
-          {/* <React.Suspense fallback={<Loader />}>
-            <AppComponent />
-          </React.Suspense> */}
           <App />
         </StaticRouter>
       </Provider>
